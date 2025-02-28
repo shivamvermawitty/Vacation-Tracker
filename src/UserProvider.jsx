0f00e00d67e0f493ad161d688b6231841337821f
Shivam Verma <shivam.verma@wittybrains.com>
2025-02-28 11:43:12 +0530

    Changes on 28th Feb

--- src/UserProvider.jsx
+++ src/UserProvider.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { UserContext } from './UserContext';
+import { getStorage } from './storageMethod';
 
 export const UserProvider = ({ children }) => {
   const [userDetails, setUserDetails] = useState(null);
-  const [userToken, setUserToken] = useState(null);
+  const [userToken, setUserToken] = useState(getStorage('authToken'));
 
   return (
     <UserContext.Provider
